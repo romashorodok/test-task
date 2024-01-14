722dd5165694da798ac94005465459105ac36462
romashorodok <108732029+romashorodok@users.noreply.github.com>
2024-01-14 18:04:29 +0200

    feat: added file management
    - parse cli files input
    - different file handling strategy for local and remote files

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"errors"
+	"flag"
+	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"regexp"
 	"sync"
 	"sync/atomic"
 	"unicode/utf8"
@@ -29,8 +33,6 @@ func isSpace(r rune) bool {
 		return false
 	}
 }
-
-var ErrNotFoundSpace = errors.New("Not found space")
 
 func (t *tokenizer) read() (int, []byte, error) {
 	if len(t.oldBuf) != 0 {
@@ -64,21 +66,19 @@ func (t *tokenizer) NextWordLen() ([]byte, int, error) {
 
 	for {
 		n, buf, err := t.read()
-		if err != nil {
-			t.buf = nil
-			t.oldBuf = nil
-			log.Println(err)
+		if err != nil && buf == nil {
 			return nil, -1, err
 		}
 
 		if n == 0 {
-			break
+			return result, wordLen, io.EOF
 		}
 
 		var r rune
 		var step int
 
 		for offset := 0; offset < n; offset += step {
+
 			r, step = utf8.DecodeRune(buf[offset:])
 			if step == 0 {
 				return nil, -1, err
@@ -93,8 +93,6 @@ func (t *tokenizer) NextWordLen() ([]byte, int, error) {
 			wordLen += step
 		}
 	}
-
-	return result, wordLen, nil
 }
 
 func NewTokenizer(source io.Reader) *tokenizer {
@@ -102,33 +100,197 @@ func NewTokenizer(source io.Reader) *tokenizer {
 		source: source,
 		// 1 byte = uint8
 		// buf: make([]byte, 1024),
-		buf: make([]byte, 24),
-		// buf: make([]byte, 2),
+		// buf: make([]byte, 24),
+		buf: make([]byte, 2),
 	}
 }
 
-func main() {
-	file, err := os.OpenFile("input_text", os.O_RDONLY, os.ModeType)
-	if err != nil {
-		notExistFile := errors.Is(err, os.ErrNotExist)
-		log.Println(notExistFile)
+type StringArrayVar []string
+
+func (s *StringArrayVar) Set(val string) error {
+	*s = append(*s, val)
+	return nil
+}
+
+func (s *StringArrayVar) String() string {
+	return fmt.Sprint(*s)
+}
+
+var _ flag.Value = (*StringArrayVar)(nil)
+
+type LocalFile struct {
+	path string
+	file *os.File
+}
+
+func (f *LocalFile) Close() error {
+	if f.file == nil {
+		return errors.New("file not exist")
 	}
-	defer file.Close()
+	return f.file.Close()
+}
 
-	tokenizer := NewTokenizer(file)
-	var counter atomic.Uint64
+func (f *LocalFile) Read(p []byte) (n int, err error) {
+	return f.file.Read(p)
+}
 
-	for {
-		word, wordLen, err := tokenizer.NextWordLen()
+var _ io.ReadCloser = (*LocalFile)(nil)
+
+func NewLocalFile(path string) (*LocalFile, error) {
+	file, err := os.OpenFile(path, os.O_RDONLY, os.ModeType)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LocalFile{
+		path: path,
+		file: file,
+	}, nil
+}
+
+type RemoteFile struct {
+	path      string
+	respErr   error
+	content   io.ReadCloser
+	contentWg sync.WaitGroup
+}
+
+func (f *RemoteFile) Close() error {
+	if f.content == nil {
+		return errors.New("content not exist")
+	}
+	return f.content.Close()
+}
+
+func (f *RemoteFile) Read(p []byte) (n int, err error) {
+	f.contentWg.Wait()
+	if f.respErr != nil || f.content == nil {
+		return 0, f.respErr
+	}
+	return f.content.Read(p)
+}
+
+var _ io.ReadCloser = (*RemoteFile)(nil)
+
+func NewRemoteFile(path string) (*RemoteFile, error) {
+	f := &RemoteFile{path: path}
+
+	go func() {
+		defer f.contentWg.Done()
+		f.contentWg.Add(1)
+
+		resp, err := http.Get(path)
 		if err != nil {
-			break
+			f.respErr = err
+			return
 		}
-		if wordLen == 0 {
+
+		if resp.StatusCode != http.StatusOK {
+			// NOTE: Server may return long response message too
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				f.respErr = err
+			}
+			f.respErr = fmt.Errorf("%s", body)
+			resp.Body.Close()
+			return
+		}
+
+		f.content = resp.Body
+	}()
+
+	return f, nil
+}
+
+var httpUrlRegex = regexp.MustCompile(`^https?://[a-zA-Z0-9.-]+(:\d{1,5})?([/?].*)?$`)
+
+func IsRemoteFile(path string) bool {
+	return httpUrlRegex.MatchString(path)
+}
+
+func BatchExec[T any](vals []T, batchSize int, fn func(T)) {
+	var wg sync.WaitGroup
+	workerPool := make(chan struct{}, batchSize)
+	defer close(workerPool)
+	for i := 0; i < batchSize; i++ {
+		workerPool <- struct{}{}
+	}
+
+	idxCh := make(chan int, batchSize)
+	defer close(idxCh)
+
+	go func() {
+		for {
+			select {
+			case idx := <-idxCh:
+				_, _ = <-workerPool
+				go func(idx int) {
+					defer func() {
+						workerPool <- struct{}{}
+						wg.Done()
+					}()
+					fn(vals[idx])
+				}(idx)
+			}
+		}
+	}()
+
+	for idx := range vals {
+		wg.Add(1)
+		idxCh <- idx
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	var files StringArrayVar
+	flag.Var(&files, "file", "Select a file to process")
+	flag.Parse()
+
+	var readers []io.ReadCloser
+
+	for _, path := range files {
+		if IsRemoteFile(path) {
+			file, err := NewRemoteFile(path)
+			if err != nil {
+				log.Printf("Skip file `%s` remote file. Err: %s", path, err)
+				continue
+			}
+			readers = append(readers, file)
+			defer file.Close()
 			continue
 		}
 
-		log.Println(wordLen, string(word))
-		counter.Add(1)
+		file, err := NewLocalFile(path)
+		if err != nil {
+			log.Printf("Skip file `%s` local file. Err: %s", path, err)
+			continue
+		}
+		readers = append(readers, file)
+		defer file.Close()
 	}
-	log.Println(counter.Load())
+
+	BatchExec(readers, 1, func(reader io.ReadCloser) {
+		defer reader.Close()
+
+		tokenizer := NewTokenizer(reader)
+		var counter atomic.Uint64
+
+		for {
+			word, wordLen, err := tokenizer.NextWordLen()
+			if err != nil {
+				log.Println(err)
+				break
+			}
+			if wordLen == 0 {
+				continue
+			}
+
+			log.Println(wordLen, string(word))
+			counter.Add(1)
+		}
+
+		log.Println(counter.Load())
+	})
 }
